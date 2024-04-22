5d319d79a59cd52f0e7a0dcd86886179ed8cc7f2
trevdev <trevorwrightdev@gmail.com>
2024-04-22 10:31:59 -0700

    fiexd authentication issue

--- frontend/app/app/page.tsx
+++ frontend/app/app/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/utils/supabase/client'
+import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
 export default async function App() {
