73acec298b6714c7b341e8cb04e39714d0479cd2
trevdev <trevorwrightdev@gmail.com>
2024-04-24 11:44:01 -0700

    set up a grid

--- frontend/utils/pixi/App.ts
+++ frontend/utils/pixi/App.ts
@@ -1,8 +1,16 @@
 import * as PIXI from 'pixi.js'
+import { TileMap , getDefaultTileMap} from './types'
+
+PIXI.TextureStyle.defaultOptions.scaleMode = 'nearest'
 
 export class App {
     private initialized: boolean = false
     private app: PIXI.Application = new PIXI.Application()
+    private tileMapContainer: PIXI.Container = new PIXI.Container({
+        scale: 5,
+    })
+    private gridLines: PIXI.Container = new PIXI.Container()
+    private tileMap: TileMap = getDefaultTileMap()
 
     async init() {
         const container = document.getElementById('app-container')
@@ -12,13 +20,16 @@ export class App {
 
         await this.app.init({
             resizeTo: container,
-        backgroundColor: 0x0F0F0F
+            backgroundColor: 0x0F0F0F,
         })
         this.initialized = true
 
+        // Mount containers
+        this.app.stage.addChild(this.tileMapContainer)
+        this.app.stage.addChild(this.gridLines)
+
         await this.loadAssets()
-        const sprite = PIXI.Sprite.from('/sprites/tile-outline.png')
-        this.app.stage.addChild(sprite)
+        this.drawGridLines()
     }
 
     getApp() {
@@ -38,4 +49,16 @@ export class App {
     async loadAssets() {
         await PIXI.Assets.load('/sprites/tile-outline.png')
     }
+
+    drawGridLines() {
+        for (let y = 0; y < this.tileMap.length; y++) {
+            for (let x = 0; x < this.tileMap[y].length; x++) {
+                const sprite = PIXI.Sprite.from('/sprites/tile-outline.png');
+                sprite.x = x * 80;
+                sprite.y = y * 80;
+                this.gridLines.addChild(sprite);
+            }
+        }
+    }
+
 }
