9025a75d79a365aee8130a3649bbd35a8fbada46
trevdev <trevorwrightdev@gmail.com>
2024-05-10 13:11:24 -0700

    nice

--- frontend/components/Modal/DeleteRoomModal.tsx
+++ frontend/components/Modal/DeleteRoomModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Modal from './Modal'
 import { useModal } from '@/app/hooks/useModal'
+import signal from '@/utils/signal'
 
 type DeleteRoomModalProps = {
     
@@ -8,12 +9,25 @@ type DeleteRoomModalProps = {
 
 const DeleteRoomModal:React.FC<DeleteRoomModalProps> = () => {
     
-    const { modal, roomToDelete } = useModal()
+    const { modal, roomToDelete, setModal } = useModal()
+
+    const onClickDelete = () => {
+        setModal('None')
+        signal.emit('deleteRoom', roomToDelete.index)   
+    }
+
+    const onClickCancel = () => {
+        setModal('None')
+    }
 
     return (
         <Modal open={modal === 'Delete Room'} closeOnOutsideClick>
-            <div className='p-2 flex flex-col items-center'>
+            <div className='p-2 flex flex-col items-center gap-2'>
                 <h1 className='text-center'>Are you sure you want to delete <span className='text-red-500'>{roomToDelete.name}</span>? It will be gone forever!</h1>
+                <div className='flex flex-row items-center gap-2'>
+                    <button className='bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-md outline-none' onClick={onClickDelete}>Delete</button>
+                    <button className='bg-secondaryhoverdark hover:bg-secondaryhover text-white px-2 py-1 rounded-md outline-none' onClick={onClickCancel}>Cancel</button>
+                </div>
             </div>
         </Modal>
     )
