b027f20b4a0c99b55f40be630e9bbfa674478cad
trevdev <trevorwrightdev@gmail.com>
2024-05-09 21:04:35 -0700

    asdas

--- frontend/app/editor/Rooms.tsx
+++ frontend/app/editor/Rooms.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { RealmData } from '@/utils/pixi/types'
 import { PlusCircleIcon } from '@heroicons/react/24/outline'
 import BasicButton from '@/components/BasicButton'
@@ -12,12 +12,15 @@ const Rooms:React.FC<RoomsProps> = ({ realmData }) => {
 
     const [rooms, setRooms] = useState<string[]>(realmData.map(room => room.name))
     const [roomIndex, setRoomIndex] = useState<number>(0)
+    const roomsContainerRef = useRef<HTMLDivElement>(null)
 
     function onClickCreateRoom() {
         signal.emit('createRoom')
     }
 
     useEffect(() => {
+        // scroll when new room is created
+        roomsContainerRef.current?.scrollTo(0, roomsContainerRef.current.scrollHeight)
         const onNewRoom = (newRoom: string) => {
             setRooms([...rooms, newRoom])
         }
@@ -28,11 +31,11 @@ const Rooms:React.FC<RoomsProps> = ({ realmData }) => {
             signal.off('newRoom', onNewRoom)
         }
     }, [rooms])
-    
+
     return (
         <div className='flex flex-col items-center px-4 grow gap-2'>
                 <h1>Rooms</h1>
-                <div className='flex flex-col items-center w-full overflow-y-scroll h-[200px] gap-1'>
+                <div className='flex flex-col items-center w-full overflow-y-scroll h-[200px] gap-1' ref={roomsContainerRef}>
                     {rooms.map((room, index) => (
                         <div className={`${roomIndex === index ? 'bg-secondaryhover' : 'bg-secondaryhoverdark'} w-full p-1 px-2 rounded-md cursor-pointer`} key={room + index}>{room}</div>
                     ))}
