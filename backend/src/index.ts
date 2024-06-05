f389d2d9b2f19a4db7484da68a46b511206ff03f
trevdev <trevorwrightdev@gmail.com>
2024-06-04 17:15:13 -0700

    added some socketio code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import http from 'http'
+import { Server as SocketIOServer } from 'socket.io'
 
 require('dotenv').config()
 
@@ -11,9 +12,18 @@ app.use(cors({
     origin: process.env.FRONTEND_URL
 }))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Initialize Socket.IO server
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL
+  }
 })
+
+// Handle a connection
+io.on('connection', (socket) => {
+    console.log('user connected')
+})
+
 
 const PORT = process.env.PORT || 3001
 server.listen(PORT, () => {
