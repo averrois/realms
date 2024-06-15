cb9ba3b7222223d5edb4548adbb9b7d8b8293253
trevorwrightdev <trevor.wright.710@gmail.com>
2024-06-15 15:42:28 -0700

    added log out button

--- frontend/app/play/PlayNavbar.tsx
+++ frontend/app/play/PlayNavbar.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { TShirt } from '@phosphor-icons/react'
 import { useModal } from '../hooks/useModal'
 import signal from '@/utils/signal'
+import { ArrowLeftEndOnRectangleIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 type PlayNavbarProps = {
     
@@ -17,7 +19,10 @@ const PlayNavbar:React.FC<PlayNavbarProps> = () => {
     }
     
     return (
-        <div className='bg-secondary w-full sm:w-[600px] md:w-[750px] lg:w-[950px] h-14 sm:absolute sm:bottom-0 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:rounded-tl-2xl sm:rounded-tr-2xl flex flex-row items-center px-2'>
+        <div className='bg-secondary w-full sm:w-[600px] md:w-[750px] lg:w-[950px] h-14 sm:absolute sm:bottom-0 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:rounded-tl-2xl sm:rounded-tr-2xl flex flex-row items-center px-2 justify-between'>
+            <Link href='/app' className='hover:bg-secondaryhover aspect-square grid place-items-center rounded-lg p-1 border-2 outline-none'>
+                <ArrowLeftEndOnRectangleIcon className='h-8 w-8'/>
+            </Link>
             <button className='hover:bg-secondaryhover aspect-square grid place-items-center rounded-lg p-1 border-2 outline-none' onClick={onClickSkinButton}>
                 <TShirt className='h-8 w-8'/>
             </button>
