1caf0f415d148a7cd553f050af149a9554805a88
trevorwrightdev <trevor.wright.710@gmail.com>
2024-06-19 23:17:33 -0700

    set up a nice loading button for link page

--- frontend/app/link/LinkClient.tsx
+++ frontend/app/link/LinkClient.tsx
@@ -2,9 +2,9 @@
 import Link from 'next/link'
 import React, { useState } from 'react'
 import LinkRealmDropdown from '@/components/LinkRealmDropdown'
-import BasicButton from '@/components/BasicButton'
 import { createClient } from '@/utils/supabase/client'
 import { toast } from 'react-toastify'
+import LoadingSpinner from '@/components/LoadingSpinner'
 
 type LinkClientProps = {
     serverName: string,
@@ -15,6 +15,7 @@ type LinkClientProps = {
 const LinkClient:React.FC<LinkClientProps> = ({ serverName, serverId, ownedRealms }) => {
     
     const [selectedRealm, setSelectedRealm] = useState(ownedRealms?.[0] ?? null)
+    const [loading, setLoading] = useState(false)
 
     function getRealmTitle() {
         if (selectedRealm) {
@@ -47,15 +48,22 @@ const LinkClient:React.FC<LinkClientProps> = ({ serverName, serverId, ownedRealm
                     <div className='flex flex-col items-center gap-2'>
                         <h1 className='3xl font-bold'>What will happen:</h1>
                         <ul className='list-disc max-w-[450px]'>
-                            <li>Messages will be shared between the Discord channels and realm rooms that you link together.</li>
+                            <li>Messages will be shared between the Discord channels and rooms that you link together.</li>
                             <li>It basically combines your realm chat and your Discord chat!</li>
                         </ul>
                         <h1 className='mt-12'>Choose a realm to link to your server!</h1>
                         <h1 className='max-w-[350px] text-center'></h1>
                         <LinkRealmDropdown realms={ownedRealms} setSelectedRealm={setSelectedRealm} selectedRealm={selectedRealm} />
-                        <BasicButton className='font-bold mt-12 p-2' onClick={onLink}>
-                            Link 🚀
-                        </BasicButton>
+                        <button className={`bg-quaternary hover:bg-quaternaryhover py-1 px-2 rounded-3xl relative`} onClick={onLink}>
+                            <div className={`${loading ? 'opacity-0' : ''} `}>
+                                Link 🚀
+                            </div>
+                            {loading && (
+                                <div className='grid place-items-center absolute w-full h-full top-0 left-0'>
+                                    <LoadingSpinner small/>
+                                </div>
+                            )}
+                        </button>
                     </div>
                 )}
             </div>
