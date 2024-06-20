76f4745869266b913470f70f1cd5b801c89ddbcc
trevorwrightdev <trevor.wright.710@gmail.com>
2024-06-19 21:16:53 -0700

    link page

--- frontend/app/link/LinkClient.tsx
+++ frontend/app/link/LinkClient.tsx
@@ -1,7 +1,8 @@
 'use client'
-import Dropdown from '@/components/Dropdown'
 import Link from 'next/link'
 import React, { useState } from 'react'
+import LinkRealmDropdown from '@/components/LinkRealmDropdown'
+import BasicButton from '@/components/BasicButton'
 
 type LinkClientProps = {
     serverName: string,
@@ -11,11 +12,11 @@ type LinkClientProps = {
 
 const LinkClient:React.FC<LinkClientProps> = ({ serverName, serverId, ownedRealms }) => {
     
-    const [selectedRealm, setSelectedRealm] = useState(ownedRealms && ownedRealms[0] ? ownedRealms[0].name : '')
+    const [selectedRealm, setSelectedRealm] = useState(ownedRealms && ownedRealms[0] ? ownedRealms[0] : null)
 
     function getRealmTitle() {
         if (selectedRealm) {
-            return <span className='text-3xl font-bold text-quaternaryhover'>{selectedRealm}</span>
+            return <span className='text-3xl font-bold text-quaternaryhover'>{selectedRealm.name}</span>
         } else {
             return <span>a realm.</span>
         }
@@ -30,8 +31,18 @@ const LinkClient:React.FC<LinkClientProps> = ({ serverName, serverId, ownedRealm
                 )}
                 {ownedRealms && ownedRealms.length > 0 && (
                     <div className='flex flex-col items-center gap-2'>
-                        <h1>Choose a realm to link to your server.</h1>
-                        <Dropdown items={ownedRealms.map((realm: any) => realm.name)} setSelectedItem={setSelectedRealm} selectedItem={selectedRealm} alternateStyle/>
+                        <h1 className='3xl font-bold'>What will happen:</h1>
+                        <ul className='list-disc'>
+                            <li>Messages from your realm will be shared with your Discord server.</li>
+                            <li>Messages from your Discord server will be shared with your realm.</li>
+                            <li>This happens by linking channels with rooms from your realm.</li>
+                        </ul>
+                        <h1 className='mt-12'>Choose a realm to link to your server!</h1>
+                        <h1 className='max-w-[350px] text-center'></h1>
+                        <LinkRealmDropdown realms={ownedRealms} setSelectedRealm={setSelectedRealm} selectedRealm={selectedRealm} />
+                        <BasicButton className='font-bold mt-12 p-2'>
+                            Link
+                        </BasicButton>
                     </div>
                 )}
             </div>
