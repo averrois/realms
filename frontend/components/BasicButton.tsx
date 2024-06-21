b87dc0aab93bd620e51ad84577cbf4debcb4fa6c
trevorwrightdev <trevor.wright.710@gmail.com>
2024-06-20 23:46:06 -0700

    added loading state to basic button component. dont overthink it its technically pointless

--- frontend/components/BasicButton.tsx
+++ frontend/components/BasicButton.tsx
@@ -1,17 +1,39 @@
 import React from 'react'
+import LoadingSpinner from './LoadingSpinner'
 
 type BasicButtonProps = {
     children?: React.ReactNode
     className?: string
     onClick?: () => void
     disabled?: boolean
+    loading?: boolean
 }
 
-const BasicButton:React.FC<BasicButtonProps> = ({ children, className, onClick, disabled }) => {
+const BasicButton:React.FC<BasicButtonProps> = ({ children, className, onClick, disabled, loading }) => {
     
     return (
-        <button className={`bg-quaternary hover:bg-quaternaryhover py-1 px-2 rounded-3xl ${disabled ? 'bg-quaternarydisabled pointer-events-none text-gray-400' : ''} ${className}`} onClick={onClick}>
-            {children}
+        <button 
+            disabled={loading || disabled} 
+            className={`
+                bg-quaternary 
+                hover:bg-quaternaryhover 
+                py-1 
+                px-2 
+                rounded-3xl 
+                relative
+                ${disabled ? 'bg-quaternarydisabled text-gray-400' : ''} 
+                ${className}
+            `} 
+                onClick={onClick}
+        >
+            <div className={`${loading ? 'opacity-0' : 'opacity-100'} `}>
+                {children}
+            </div>
+            {loading && (
+                <div className='grid place-items-center absolute w-full h-full top-0 left-0'>
+                    <LoadingSpinner small/>
+                </div>
+            )}
         </button>
     )
 }
--- frontend/components/Navbar/NavbarChild.tsx
+++ frontend/components/Navbar/NavbarChild.tsx
@@ -11,14 +11,16 @@ type NavbarChildProps = {
 
 export const NavbarChild:React.FC<NavbarChildProps> = ({ name, avatar_url }) => {
 
-    const { modal, setModal } = useModal()
+    const { setModal } = useModal()
 
     return (
         <div className='h-16'>
             <div className='w-full fixed bg-secondary flex flex-row items-center p-2 pl-4 justify-end sm:justify-between z-10'>
-                <BasicButton onClick={() => setModal('Create Realm')} className='hidden sm:flex flex-row items-center gap-2 text-lg'>
-                    Create Realm
-                    <PlusCircleIcon className='h-5'/>
+                <BasicButton onClick={() => setModal('Create Realm')} className='hidden sm:flex'>
+                    <div className='flex flex-row items-center gap-2 text-lg'>
+                        Create Realm
+                        <PlusCircleIcon className='h-5'/>
+                    </div>
                 </BasicButton>
                 <div className='flex flex-row items-center gap-4 hover:bg-lightblue rounded-full cursor-pointer py-1 px-1 select-none' onClick={() => setModal('Account Dropdown')}>
                     <p className='text-white text-lg'>{name}</p>
