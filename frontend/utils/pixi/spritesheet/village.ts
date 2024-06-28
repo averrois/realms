0151e438f361adf4496be4de245a2dd198b9f7d5
trevorwrightdev <trevor.wright.710@gmail.com>
2024-06-28 16:47:10 -0700

    benches

--- frontend/utils/pixi/spritesheet/village.ts
+++ frontend/utils/pixi/spritesheet/village.ts
@@ -64,11 +64,35 @@ const sprites: SpriteSheetTile[] = [
     { name: 'other_chair_up', x: 96, y: 160, width: 32, height: 32, layer: 'above_floor' },
     { name: 'small_stool', x: 128, y: 128, width: 32, height: 32, layer: 'above_floor' },
 
+    { name: 'bench_right_top', x: 0, y: 192, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'bench_left_top', x: 32, y: 192, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'empty', x: 0, y: 0, width: 0, height: 0, layer: 'above_floor' },
+    { name: 'bench_right_bottom', x: 0, y: 224, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'bench_left_bottom', x: 32, y: 224, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'empty', x: 0, y: 0, width: 0, height: 0, layer: 'above_floor' },
+    { name: 'hole_bench_left', x: 64, y: 192, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'hole_bench_right', x: 96, y: 192, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'empty', x: 0, y: 0, width: 0, height: 0, layer: 'above_floor' },
+    { name: 'hole_bench_left_up', x: 128, y: 192, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'hole_bench_right_up', x: 160, y: 192, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'empty', x: 0, y: 0, width: 0, height: 0, layer: 'above_floor' },
+    { name: 'bench_left', x: 64, y: 224, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'bench_right', x: 96, y: 224, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'empty', x: 0, y: 0, width: 0, height: 0, layer: 'above_floor' },
+    { name: 'bench_left_up', x: 128, y: 224, width: 32, height: 32, layer: 'above_floor' },
+    { name: 'bench_right_up', x: 160, y: 224, width: 32, height: 32, layer: 'above_floor' },
+
     { name: 'sign_1', x: 224, y: 96, width: 32, height: 64, layer: 'object', colliders: [{ x: 0, y: 1 }] },
     { name: 'sign_2', x: 256, y: 96, width: 32, height: 64, layer: 'object', colliders: [{ x: 0, y: 1 }] },
     { name: 'sign_3', x: 288, y: 96, width: 32, height: 64, layer: 'object', colliders: [{ x: 0, y: 1 }] },
     { name: 'sign_4', x: 320, y: 96, width: 32, height: 64, layer: 'object', colliders: [{ x: 0, y: 1 }] },
 
+    { name: 'mailbox', x: 224, y: 160, width: 32, height: 32, layer: 'object', colliders: [{ x: 0, y: 0 }] },
+    { name: 'crate', x: 256, y: 160, width: 32, height: 32, layer: 'object', colliders: [{ x: 0, y: 0 }] },
+    { name: 'small_crate', x: 256, y: 192, width: 32, height: 32, layer: 'object', colliders: [{ x: 0, y: 0 }] },
+    { name: 'box', x: 288, y: 160, width: 32, height: 32, layer: 'object', colliders: [{ x: 0, y: 0 }] },
+    { name: 'empty_box', x: 320, y: 160, width: 32, height: 32, layer: 'object', colliders: [{ x: 0, y: 0 }] },
+    { name: 'small_empty_box', x: 320, y: 192, width: 32, height: 32, layer: 'object', colliders: [{ x: 0, y: 0 }] },
 ]
 
 const villageSpriteSheetData = new SpriteSheetData(width, height, url, sprites)
